fdf594709b6d4ef6cb8328872b0744f54f0b5a2e
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-05-07 16:29:05 -0300

    Nest components in App

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 import React from "react";
 import BookContextProvider from "contexts/BookContext";
+import NavBar from "./NavBar";
+import styled from "styled-components";
+import BookList from "./BookList";
+
+const Container = styled.div`
+  background-color: #4c2a4c;
+  margin: 20px auto;
+  width: 90%;
+  max-width: 700px;
+  color: #eee;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <BookContextProvider></BookContextProvider>
-    </div>
+    <Container>
+      <BookContextProvider>
+        <NavBar />
+        <BookList />
+      </BookContextProvider>
+    </Container>
   );
 }
 
