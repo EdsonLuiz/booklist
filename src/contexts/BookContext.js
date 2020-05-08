9644ad7a0a2e3df101ab3f9a4037c92ab254505a
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-05-08 09:15:56 -0300

    Remove dummy data

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,16 +1,10 @@
 import React, { createContext, useReducer } from "react";
-import { v4 as uuidv4 } from "uuid";
 import { bookReducer } from "reducers/bookReducer";
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-  const [books, dispatch] = useReducer(bookReducer, [
-    { id: uuidv4(), title: "Title of book 01", author: "Some author" },
-    { id: uuidv4(), title: "Title of book 02", author: "Some author" },
-    { id: uuidv4(), title: "Title of book 03", author: "Some author" },
-    { id: uuidv4(), title: "Title of book 04", author: "Some author" },
-  ]);
+  const [books, dispatch] = useReducer(bookReducer, []);
 
   return (
     <BookContext.Provider value={{ books, dispatch }}>
